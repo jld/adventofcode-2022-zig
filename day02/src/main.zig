399c410b1e6d9a91f6cbd6ce377dfa362497fde6
Jed Davis <jld@xlerb.net>
2022-12-02 22:04:15 -0800

    day02: Rewrite everything.
    Weird enum math validated with exhaustive testing, plus a semi-generic type.

--- day02/src/main.zig
+++ day02/src/main.zig
@@ -1,145 +1,184 @@
 const std = @import("std");
 
-const Shape = enum { rock, paper, scissors };
+const Shape = enum(u3) {
+    rock,
+    paper,
+    scissors,
 
-fn parse_shape(comptime key: *const [3]u8, c: u8) !Shape {
-    return switch (c) {
-        key[0] => .rock,
-        key[1] => .paper,
-        key[2] => .scissors,
-        else => error.BadCharacter,
-    };
+    fn parse(comptime key: *const [3]u8, c: u8) !Shape {
+        return switch (c) {
+            key[0] => .rock,
+            key[1] => .paper,
+            key[2] => .scissors,
+            else => error.BadCharacter,
+        };
+    }
+};
+
+test "parse_shape_abc" {
+    const t = std.testing;
+    try t.expectEqual(Shape.rock, try Shape.parse("ABC", 'A'));
+    try t.expectEqual(Shape.paper, try Shape.parse("ABC", 'B'));
+    try t.expectEqual(Shape.scissors, try Shape.parse("ABC", 'C'));
+    try t.expectError(error.BadCharacter, Shape.parse("ABC", 'X'));
 }
 
-test "parse_abc" {
+test "parse_shape_xyz" {
     const t = std.testing;
-    try t.expectEqual(Shape.rock, try parse_shape("ABC", 'A'));
-    try t.expectEqual(Shape.paper, try parse_shape("ABC", 'B'));
-    try t.expectEqual(Shape.scissors, try parse_shape("ABC", 'C'));
-    try t.expectError(error.BadCharacter, parse_shape("ABC", 'X'));
+    try t.expectEqual(Shape.rock, try Shape.parse("XYZ", 'X'));
+    try t.expectEqual(Shape.paper, try Shape.parse("XYZ", 'Y'));
+    try t.expectEqual(Shape.scissors, try Shape.parse("XYZ", 'Z'));
+    try t.expectError(error.BadCharacter, Shape.parse("XYZ", 'A'));
 }
 
-test "parse_xyz" {
+const End = enum(u3) {
+    lose,
+    draw,
+    win,
+
+    fn parse(comptime key: *const [3]u8, c: u8) !End {
+        return switch (c) {
+            key[0] => .lose,
+            key[1] => .draw,
+            key[2] => .win,
+            else => error.BadCharacter,
+        };
+    }
+};
+
+test "parse_end_xyz" {
     const t = std.testing;
-    try t.expectEqual(Shape.rock, try parse_shape("XYZ", 'X'));
-    try t.expectEqual(Shape.paper, try parse_shape("XYZ", 'Y'));
-    try t.expectEqual(Shape.scissors, try parse_shape("XYZ", 'Z'));
-    try t.expectError(error.BadCharacter, parse_shape("XYZ", 'A'));
+    try t.expectEqual(End.lose, try End.parse("XYZ", 'X'));
+    try t.expectEqual(End.draw, try End.parse("XYZ", 'Y'));
+    try t.expectEqual(End.win, try End.parse("XYZ", 'Z'));
+    try t.expectError(error.BadCharacter, End.parse("XYZ", 'A'));
+}
+
+fn match_fwd(me: Shape, them: Shape) End {
+    return @intToEnum(End, (@enumToInt(me) + 4 - @enumToInt(them)) % 3);
+}
+
+fn match_rev(them: Shape, end: End) Shape {
+    return @intToEnum(Shape, (@enumToInt(them) + 2 + @enumToInt(end)) % 3);
+}
+
+const validation = .{
+    .{ Shape.rock, Shape.rock, End.draw },
+    .{ Shape.paper, Shape.paper, End.draw },
+    .{ Shape.scissors, Shape.scissors, End.draw },
+    .{ Shape.rock, Shape.scissors, End.win },
+    .{ Shape.scissors, Shape.paper, End.win },
+    .{ Shape.paper, Shape.rock, End.win },
+    .{ Shape.scissors, Shape.rock, End.lose },
+    .{ Shape.paper, Shape.scissors, End.lose },
+    .{ Shape.rock, Shape.paper, End.lose },
+};
+
+test "match_fwd" {
+    const t = std.testing;
+    inline for (validation) |testcase| {
+        try t.expectEqual(testcase[2], match_fwd(testcase[0], testcase[1]));
+    }
+}
+
+test "match_rev" {
+    const t = std.testing;
+    inline for (validation) |testcase| {
+        try t.expectEqual(testcase[0], match_rev(testcase[1], testcase[2]));
+    }
 }
 
 const score_t = i64;
 
-const Round = struct {
-    them: Shape,
-    me: Shape,
-
-    fn won(self: Round) bool {
-        const wins = [_]Round{
-            .{ .me = .rock, .them = .scissors },
-            .{ .me = .scissors, .them = .paper },
-            .{ .me = .paper, .them = .rock },
-        };
-        inline for (wins) |win| {
-            if (std.meta.eql(self, win)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    fn swap(self: Round) Round {
-        return .{ .them = self.me, .me = self.them };
-    }
-
-    fn lost(self: Round) bool {
-        return self.swap().won();
-    }
-
-    fn score(self: Round) score_t {
-        const tiebreaker: score_t = switch (self.me) {
-            .rock => 1,
-            .paper => 2,
-            .scissors => 3,
-        };
-        const outcome: score_t = if (self.won()) 6 else if (self.lost()) 0 else 3;
-        return outcome + tiebreaker;
-    }
-};
-
-test "round_score" {
-    const t = std.testing;
-    try t.expectEqual(@as(score_t, 8), (Round{ .them = .rock, .me = .paper }).score());
-    try t.expectEqual(@as(score_t, 1), (Round{ .them = .paper, .me = .rock }).score());
-    try t.expectEqual(@as(score_t, 6), (Round{ .them = .scissors, .me = .scissors }).score());
+fn score_round(them: Shape, me: Shape) score_t {
+    return 1 + @enumToInt(me) + @as(score_t, 3) * @enumToInt(match_fwd(me, them));
 }
 
-fn parse_round(input: []const u8) !Round {
-    if (input.len != 3) {
-        return error.BadSyntax;
-    }
-    if (input[1] != ' ') {
-        return error.BadSyntax;
-    }
-    return .{
-        .them = try parse_shape("ABC", input[0]),
-        .me = try parse_shape("XYZ", input[2]),
+test "score_round" {
+    const t = std.testing;
+
+    try t.expectEqual(@as(score_t, 8), score_round(.rock, .paper));
+    try t.expectEqual(@as(score_t, 1), score_round(.paper, .rock));
+    try t.expectEqual(@as(score_t, 6), score_round(.scissors, .scissors));
+
+    try t.expectEqual(@as(score_t, 4), score_round(.rock, .rock));
+    try t.expectEqual(@as(score_t, 7), score_round(.scissors, .rock));
+}
+
+fn GuideLine(comptime TR: type) type {
+    return struct {
+        const This = @This();
+
+        lhs: Shape,
+        rhs: TR,
+
+        fn parse(input: []const u8) !This {
+            if (input.len != 3) {
+                return error.BadSyntax;
+            }
+            if (input[1] != ' ') {
+                return error.BadSyntax;
+            }
+            return This{
+                .lhs = try Shape.parse("ABC", input[0]),
+                .rhs = try TR.parse("XYZ", input[2]),
+            };
+        }
     };
 }
 
-test "parse_round" {
+test "parse_line_p1" {
     const t = std.testing;
-    const validate = struct { // lolsigh
-        fn call(input: []const u8, exp_them: Shape, exp_me: Shape) !void {
-            const round = try parse_round(input);
-            try t.expectEqual(exp_them, round.them);
-            try t.expectEqual(exp_me, round.me);
+    const check = struct {
+        fn call(input: []const u8, lhs: Shape, rhs: Shape) !void {
+            const gl = try GuideLine(Shape).parse(input);
+            try t.expectEqual(lhs, gl.lhs);
+            try t.expectEqual(rhs, gl.rhs);
         }
     }.call;
 
-    try validate("A Y", .rock, .paper);
-    try validate("B X", .paper, .rock);
-    try validate("C Z", .scissors, .scissors);
+    try check("A Y", .rock, .paper);
+    try check("B X", .paper, .rock);
+    try check("C Z", .scissors, .scissors);
 }
 
-fn score_game(input: []const u8) !score_t {
-    var lines = std.mem.split(u8, std.mem.trimRight(u8, input, "\n"), "\n");
-    var acc: score_t = 0;
-    while (lines.next()) |line| {
-        acc += (try parse_round(line)).score();
-    }
-    return acc;
-}
-
-test "score_game" {
+test "parse_line_p2" {
     const t = std.testing;
-    const text =
-        \\A Y
-        \\B X
-        \\C Z
-    ;
+    const check = struct {
+        fn call(input: []const u8, lhs: Shape, rhs: End) !void {
+            const gl = try GuideLine(End).parse(input);
+            try t.expectEqual(lhs, gl.lhs);
+            try t.expectEqual(rhs, gl.rhs);
+        }
+    }.call;
 
-    try t.expectEqual(@as(score_t, 15), try score_game(text));
+    try check("A Y", .rock, .draw);
+    try check("B X", .paper, .lose);
+    try check("C Z", .scissors, .win);
 }
 
-pub fn main() !void {
-    const max_input: usize = 0x8000_0000;
-
-    const stdin_file = std.io.getStdIn().reader();
-    var br = std.io.bufferedReader(stdin_file);
-    const stdin = br.reader();
-
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
-    defer std.debug.assert(!general_purpose_allocator.deinit());
-    const gpa = general_purpose_allocator.allocator();
-
-    var input = try stdin.readAllAlloc(gpa, max_input);
-    defer gpa.free(input);
-
-    try stdout.print("{}\n", .{try score_game(input)});
-
-    try bw.flush();
+fn rscore_p1(gl: GuideLine(Shape)) score_t {
+    return score_round(gl.lhs, gl.rhs);
 }
+
+fn rscore_p2(gl: GuideLine(End)) score_t {
+    return score_round(gl.lhs, match_rev(gl.lhs, gl.rhs));
+}
+
+test "rscore_p1" {
+    const t = std.testing;
+
+    try t.expectEqual(@as(score_t, 8), rscore_p1(try GuideLine(Shape).parse("A Y")));
+    try t.expectEqual(@as(score_t, 1), rscore_p1(try GuideLine(Shape).parse("B X")));
+    try t.expectEqual(@as(score_t, 6), rscore_p1(try GuideLine(Shape).parse("C Z")));
+}
+
+test "rscore_p2" {
+    const t = std.testing;
+
+    try t.expectEqual(@as(score_t, 4), rscore_p2(try GuideLine(End).parse("A Y")));
+    try t.expectEqual(@as(score_t, 1), rscore_p2(try GuideLine(End).parse("B X")));
+    try t.expectEqual(@as(score_t, 7), rscore_p2(try GuideLine(End).parse("C Z")));
+}
+
+pub fn main() !void {}
