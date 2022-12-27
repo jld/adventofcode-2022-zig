311a2bad0b7166b33deae2bb3a3316e559a5fcba
Jed Davis <jld@xlerb.net>
2022-12-27 10:39:20 -0800

    day09: Now I can drag the tail around.

--- day09/src/main.zig
+++ day09/src/main.zig
@@ -128,6 +128,67 @@ test "contract" {
     }
 }
 
+const coord_t = i32;
+const Pos = struct {
+    x: coord_t,
+    y: coord_t,
+
+    fn init(x: coord_t, y: coord_t) Pos {
+        return .{ .x = x, .y = y };
+    }
+    const zero = init(0, 0);
+
+    fn add(self: Pos, delta: Move) Pos {
+        return init(self.x + @as(coord_t, delta.dx), self.y + @as(coord_t, delta.dy));
+    }
+    fn is_at(self: Pos, ex: coord_t, ey: coord_t) bool {
+        return self.x == ex and self.y == ey;
+    }
+};
+
+const Rope1 = struct {
+    const Self = @This();
+    head: Pos,
+    tail: Move,
+
+    fn init() Self {
+        return .{ .head = Pos.zero, .tail = Move.zero };
+    }
+
+    fn move(self: *Self, mov0: Move) void {
+        self.head = self.head.add(mov0);
+        self.tail = self.tail.sub(mov0);
+        _ = self.tail.contract();
+    }
+
+    fn tail_pos(self: *const Self) Pos {
+        return self.head.add(self.tail);
+    }
+};
+
+test "rope example" {
+    const t = std.testing;
+    
+    var r = Rope1.init();
+    r.move(Move.right);
+    try t.expect(r.head.is_at(1, 0));
+    try t.expect(r.tail_pos().is_at(0, 0));
+    r.move(Move.right);
+    try t.expect(r.head.is_at(2, 0));
+    try t.expect(r.tail_pos().is_at(1, 0));
+    r.move(Move.right);
+    r.move(Move.right);
+    try t.expect(r.head.is_at(4, 0));
+    try t.expect(r.tail_pos().is_at(3, 0));
+    r.move(Move.up);
+    try t.expect(r.head.is_at(4, -1));
+    try t.expect(r.tail_pos().is_at(3, 0));
+    r.move(Move.up);
+    try t.expect(r.head.is_at(4, -2));
+    try t.expect(r.tail_pos().is_at(4, -1));
+    // That's probably enough.
+}
+
 fn io_main(ctx: util.IOContext) !void {
     _ = ctx;
 }
